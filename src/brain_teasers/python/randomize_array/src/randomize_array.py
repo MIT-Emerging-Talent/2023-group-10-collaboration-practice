27f03f7c1644326d3e8ba564a108878d26ad6f9d
Wagoory <wa8oory@gmail.com>
2024-01-20 14:33:52 +0400

    randomized_array

--- src/brain_teasers/python/randomize_array/src/randomize_array.py
+++ src/brain_teasers/python/randomize_array/src/randomize_array.py
@@ -1,6 +1,17 @@
 import random
 
 def randomize_array(original_array):
+    """
+    The randomize_array function takes an input array, copies it and shuffle the elements inside
+    to create a randomized copy of our original array
+    
+    Parameters:
+    - original_array: The array to be randomized.
+
+    Returns: A new list containing the elements of the original array in a random order.
+
+    """
+
     randomized_array = original_array.copy()
 
     random.shuffle(randomized_array)
